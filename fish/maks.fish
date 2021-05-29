401418f53266314c67e5ae110ed1a5d14e76c525
Maksym Nazarenko <maks.nazarenko@gmail.com>
2021-05-29 13:03:39 +0300

    add pip user's bin dir to Fish path

--- fish/maks.fish
+++ fish/maks.fish
@@ -5,3 +5,5 @@ set -g theme_display_git_default_branch yes
 set -g theme_display_cmd_duration yes
 set -g theme_show_exit_status yes
 set -g theme_newline_cursor yes
+
+set PATH $HOME/.local/bin $PATH
